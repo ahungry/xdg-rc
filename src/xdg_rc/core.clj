32798ef8f33d7bf8441d1cac3fa86b74a8950352
Matthew Carter <m@ahungry.com>
2019-09-01 13:53:30 -0400

    add some code

--- src/xdg_rc/core.clj
+++ src/xdg_rc/core.clj
@@ -1,5 +1,28 @@
 (ns xdg-rc.core)
 
+;; https://specifications.freedesktop.org/basedir-spec/latest/ar01s03.html
+
+(defn get-xdg-config-home
+  "Get the XDG_CONFIG_HOME or HOME/.config if its not defined."
+  []
+  (or (System/getenv "XDG_CONFIG_HOME")
+      (str (System/getProperty "user.home") "/.config")))
+
+(def get-config-home get-xdg-config-home)
+
+(defn get-rc-file-raw
+  "Read a file if it exist."
+  []
+  (let [defaults (read-string (slurp "../conf/default-rc"))
+        home-rc (format "%s/.insectariumrc" (System/getProperty "user.home"))
+        xdg-rc (format "%s/insectarium/insectariumrc" (get-config-home))]
+    (conj
+     defaults
+     (if (.exists (clojure.java.io/file home-rc))
+       (read-string (slurp home-rc)))
+     (if (.exists (clojure.java.io/file xdg-rc))
+       (read-string (slurp xdg-rc))))))
+
 (defn foo
   "I don't do a whole lot."
   [x]
